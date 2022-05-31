5102bbef46e90b4e16cbb7fc50248015a71e87ad
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-05-31 20:19:03 -0300

    Composition API: uso de useRouter y useRoute

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,26 @@ const routes = [
         component: () =>
             import(/* webpackChunkName: "users" */ '../views/Users.vue'),
     },
+    {
+        path: '/pokemon-search',
+        name: 'pokemon-search',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(
+                /* webpackChunkName: "pokemon-search" */ '../views/SearchPokemon.vue'
+            ),
+    },
+    {
+        path: '/pokemon/:id',
+        name: 'pokemon-id',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "pokemon-id" */ '../views/Pokemon.vue'),
+    },
 ]
 
 const router = createRouter({
---
+++ src/views/Pokemon.vue
@@ -0,0 +1,22 @@
+import { useRouter } from 'vue-router';
+<template>
+    <h1>Pokemon View: {{ id }}</h1>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+export default {
+    name: 'pokemon-id',
+    setup() {
+        const route = useRoute()
+        console.log(route.params)
+        const id = route.params.id
+        return {
+            id,
+        }
+    },
+}
+</script>
+
+<style>
+</style>
