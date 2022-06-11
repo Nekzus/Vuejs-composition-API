4046f414881ceb428b7549ed2258b05004ef9449
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-06-11 20:51:01 -0300

    Actualizacion codigo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,68 +6,68 @@
 </template>
 
 <script>
-import {
-    ref,
-    onMounted,
-    onUpdated,
-    onUnmounted,
-    onBeforeMount,
-    onBeforeUpdate,
-    onBeforeUnmount,
-    onErrorCaptured,
-    onRenderTracked,
-    onRenderTriggered,
-    onActivated,
-    onDeactivated,
-    onServerPrefetch,
-} from 'vue'
-export default {
-    setup() {
-        const title = ref('About')
+    import {
+        ref,
+        onMounted,
+        onUpdated,
+        onUnmounted,
+        onBeforeMount,
+        onBeforeUpdate,
+        onBeforeUnmount,
+        onErrorCaptured,
+        onRenderTracked,
+        onRenderTriggered,
+        onActivated,
+        onDeactivated,
+        onServerPrefetch,
+    } from 'vue'
+    export default {
+        setup() {
+            const title = ref('About')
 
-        console.log('Setup se llamo')
+            console.log('Setup se llamo')
 
-        onMounted(() => {
-            console.log('onMounted')
-        })
-        onUpdated(() => {
-            console.log('onUpdated')
-        })
-        onUnmounted(() => {
-            console.log('onUnmounted')
-        })
-        onBeforeMount(() => {
-            console.log('onBeforeMount')
-        })
-        onBeforeUpdate(() => {
-            console.log('onBeforeUpdate')
-        })
-        onBeforeUnmount(() => {
-            console.log('onBeforeUnmount')
-        })
-        onErrorCaptured(() => {
-            console.log('onErrorCaptured')
-        })
-        onRenderTracked(() => {
-            console.log('onRenderTracked')
-        })
-        onRenderTriggered(() => {
-            console.log('onRenderTriggered')
-        })
-        onActivated(() => {
-            console.log('onActivated')
-        })
-        onDeactivated(() => {
-            console.log('onDeactivated')
-        })
-        onServerPrefetch(() => {
-            console.log('onServerPrefetch')
-        })
+            onMounted(() => {
+                console.log('onMounted')
+            })
+            onUpdated(() => {
+                console.log('onUpdated')
+            })
+            onUnmounted(() => {
+                console.log('onUnmounted')
+            })
+            onBeforeMount(() => {
+                console.log('onBeforeMount')
+            })
+            onBeforeUpdate(() => {
+                console.log('onBeforeUpdate')
+            })
+            onBeforeUnmount(() => {
+                console.log('onBeforeUnmount')
+            })
+            onErrorCaptured(() => {
+                console.log('onErrorCaptured')
+            })
+            onRenderTracked(() => {
+                console.log('onRenderTracked')
+            })
+            onRenderTriggered(() => {
+                console.log('onRenderTriggered')
+            })
+            onActivated(() => {
+                console.log('onActivated')
+            })
+            onDeactivated(() => {
+                console.log('onDeactivated')
+            })
+            onServerPrefetch(() => {
+                console.log('onServerPrefetch')
+            })
 
-        return {
-            title,
-            onChangeTitle: () => (title.value = 'New About'),
-        }
-    },
-}
+            return {
+                title,
+                onChangeTitle: () => (title.value = 'New About'),
+            }
+        },
+    }
 </script>
